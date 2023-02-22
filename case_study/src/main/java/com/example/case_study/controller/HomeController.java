94ad7b5a7ce8d305797052461b927bdfc174f26e
lann95081 <lann95081@gmail.com>
2023-02-22 17:19:39 +0700

    up

--- case_study/src/main/java/com/example/case_study/controller/HomeController.java
+++ case_study/src/main/java/com/example/case_study/controller/HomeController.java
@@ -10,18 +10,4 @@ public class HomeController {
         return "home";
     }
 
-    @GetMapping("/loginPage")
-    public String showLoginPage() {
-        return "/security/myLogin";
-    }
-
-    @GetMapping("/error403")
-    public String showError403() {
-        return "/security/error403";
-    }
-
-    @GetMapping("/login")
-    public String showLoginHome() {
-        return "/home";
-    }
 }
